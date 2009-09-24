9fbb9ee4fd632ebbd86ec979b169f29856f4f5a2
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-09-24 16:31:42 +0700

    Add dynamic class generation
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- lib/src/main/java/com/smartitengineering/demo/groovydynaclass/lib/App.java
+++ lib/src/main/java/com/smartitengineering/demo/groovydynaclass/lib/App.java
@@ -1,13 +1,25 @@
 package com.smartitengineering.demo.groovydynaclass.lib;
 
+import groovy.lang.GroovyClassLoader;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+		public static void main(String[] args)
+						throws Exception {
+				GroovyClassLoader classLoader = new GroovyClassLoader(Thread.
+								currentThread().getContextClassLoader());
+				Class clazz =
+							classLoader.parseClass(
+								"package com.smartitengineering.demo.dyna.embed;\n" +
+								"class Product{int getProductId() {return 0}\n" +
+								"public String toString() {return \"Embed\"}}");
+
+				System.out.println("Class = " + clazz.getName());
+				final Object newInstance = clazz.newInstance();
+				System.out.println("toString` = " + newInstance);
+		}
 }
